58e5486a11d609d78b590fbd95fb1089d051bfde
Hyuk Myeong <hyuk.myeong@lge.com>
2020-05-07 20:27:15 +0900

    emacs configuration

--- _emacs.d/init.el
+++ _emacs.d/init.el
@@ -41,7 +41,11 @@
 (add-hook 'c++-mode-hook 'helm-gtags-mode)
 (add-hook 'asm-mode-hook 'helm-gtags-mode)
 
-;; mikki start
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; mikki start				              ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq-default package-check-signature nil)
 
 ;(define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
 ;(define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
@@ -95,10 +99,10 @@
   (and evil-mode (evil-force-normal-state))
   (keyboard-quit))
 
-(define-key evil-normal-state-map   (kbd "C-g") #'evil-keyboard-quit) 
-(define-key evil-motion-state-map   (kbd "C-g") #'evil-keyboard-quit) 
-(define-key evil-insert-state-map   (kbd "C-g") #'evil-keyboard-quit) 
-(define-key evil-window-map         (kbd "C-g") #'evil-keyboard-quit) 
+(define-key evil-normal-state-map   (kbd "C-g") #'evil-keyboard-quit)
+(define-key evil-motion-state-map   (kbd "C-g") #'evil-keyboard-quit)
+(define-key evil-insert-state-map   (kbd "C-g") #'evil-keyboard-quit)
+(define-key evil-window-map         (kbd "C-g") #'evil-keyboard-quit)
 (define-key evil-operator-state-map (kbd "C-g") #'evil-keyboard-quit)
 
 ;; evil을 사용하면 helm의 M-. 가 동작하지 않기 때문에 아래 내용을 주석처리함
@@ -109,7 +113,40 @@
 ; 윈도우 이동 : shift + arrow
 (windmove-default-keybindings)
 
-;; mikki finish 
+;; scroll 부드럽게
+;; for smooth scrolling and disabling the automatical recentering of emacs when moving the cursor
+;;(setq scroll-conservatively 0
+(setq-default scroll-margin 1
+scroll-conservatively 0
+scroll-up-aggressively 0.01
+scroll-down-aggressively 0.01)
+
+;; 테마 뭘로?
+;doom-nord-light
+;(setq doom-theme 'doom-nord-light)
+
+;; block and indent
+(global-set-key (kbd "C->") 'indent-rigidly-right-to-tab-stop)
+(global-set-key (kbd "C-<") 'indent-rigidly-left-to-tab-stop)
+
+;; use space to indent by default
+(setq-default indent-tabs-mode nil)
+
+;; Package: clean-aindent-mode
+(require 'clean-aindent-mode)
+(add-hook 'prog-mode-hook 'clean-aindent-mode)
+
+;(add-hook 'c-mode-hook '
+;  (lambda ()
+;  (c-set-style "bsd")
+;  (setq tab-width 2)
+;  (setq c-basic-offset 2) ;; indent use only 2 blank
+;  (setq indent-tabs-mode nil) ;; no tab
+;))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; mikki finish 				      ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; function-args
 ;; (require 'function-args)
@@ -166,9 +203,9 @@
 ;; “python”: What Python developers use for extension modules
 ;; “java”: The default style for java-mode (see below)
 ;; “user”: When you want to define your own style
-(setq
- c-default-style "linux" ;; set style to "linux"
- )
+;(setq
+; c-default-style "k&r" ;; set style to "linux"
+; )
 
 (global-set-key (kbd "RET") 'newline-and-indent)  ; automatically indent when press RET
 
@@ -178,16 +215,6 @@
 ;; show unncessary whitespace that can mess up your diff
 (add-hook 'prog-mode-hook (lambda () (interactive) (setq show-trailing-whitespace 1)))
 
-;; use space to indent by default
-(setq-default indent-tabs-mode nil)
-
-;; set appearance of a tab that is represented by 4 spaces
-(setq-default tab-width 4)
-
-;; Package: clean-aindent-mode
-(require 'clean-aindent-mode)
-(add-hook 'prog-mode-hook 'clean-aindent-mode)
-
 (setq
  ;; use gdb-many-windows by default
  gdb-many-windows t
@@ -195,4 +222,5 @@
  ;; Non-nil means display source file containing the main routine at startup
  gdb-show-main t
  )
+
 (put 'erase-buffer 'disabled nil)
