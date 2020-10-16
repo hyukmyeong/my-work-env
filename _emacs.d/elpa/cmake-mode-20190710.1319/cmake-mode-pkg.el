2b6852a0afbdcb7ff45277f0481fd57323583ac3
Hyuk Myeong <hyuk.myeong@lge.com>
2020-10-16 16:25:14 +0900

    it was difficult

---
+++ _emacs.d/elpa/cmake-mode-20190710.1319/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/hyukmyeong/.emacs.d/elpa/cmake-mode-20190710.1319/cmake-mode.el  -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "20190710.1319" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "4f6fccd86174742c2f17aeba1a940be383a3c7df")
